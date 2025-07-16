4a699703d491aa1b12156fe4dafa9586d4c7642d
Vaishnavi-459 <vaishnavivaishuu459@gmail.com>
2025-07-16 17:10:03 +0530

    Updated_MainAdmin_New2

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'http://alb-dev-sc-197990416.ap-south-1.elb.amazonaws.com/api'
-const ipUrl = '192.168.1.24'
+const ipUrl = '192.168.1.26'
 //const ipUrl = '13.127.106.17'
 
 export const BASE_URL = `http://${ipUrl}:8081`
